2f9bcc0a4c2c04e407a1e93e763f09bee072450d
kamau-n <bsclmr146620@spu.ac.ke>
2023-02-15 22:54:09 +0300

    commit

---
+++ src/main/java/com/example/StudentApi/Security/AppSecurityConfig.java
@@ -0,0 +1,14 @@
+package com.example.StudentApi.Security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig extends WebSecurityConfiguration  {
+
+
+
+}
