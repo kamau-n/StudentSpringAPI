1f5d0531a1f72ba94905517669d2c8312aa2d104
kamau-n <bsclmr146620@spu.ac.ke>
2023-02-16 18:08:24 +0300

    commit

--- src/main/java/com/example/StudentApi/Security/AppSecurityConfig.java
+++ src/main/java/com/example/StudentApi/Security/AppSecurityConfig.java
@@ -1,14 +1,24 @@
 package com.example.StudentApi.Security;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
 
-
-@Configuration
 @EnableWebSecurity
-public class AppSecurityConfig extends WebSecurityConfiguration  {
+public class AppSecurityConfig {
 
+    @Bean
 
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+        
+      return http.build();
+
+    }
 
 }
